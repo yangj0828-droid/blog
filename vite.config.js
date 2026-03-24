6c3720a98ac8c099ed919c9530d2374033726447
yangj0828-droid <yangj0828@gmail.com>
2026-03-24 14:33:23 +0900

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/blog/',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    base: '/',
+    plugins: [react()],
+    resolve: {
+          alias: {
+                  "@": path.resolve(__dirname, "./src"),
+          },
     },
-  },
 })
